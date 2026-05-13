28f8e1ff713a2476a5f8168b8c50a36972dc0f8f
Yoshiakzu ITO <aisyu1976@gmail.com>
2026-05-14 06:43:53 +0900

    Add Go environment check

--- scripts/check_environment.sh
+++ scripts/check_environment.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 echo "=== Environment Check ==="
-
 check_command() {
     local name="$1"
     local command="$2"
+    local version_option="${3:---version}"
 
     echo -n "$name: "
 
     if command -v "$command" > /dev/null 2>&1; then
-        "$command" --version | head -n 1
+        "$command" $version_option | head -n 1
     else
         echo "Not installed"
     fi
 }
+
+
+
 
 check_command "Python" "python3"
 check_command "Python 3.12" "python3.12"
@@ -23,3 +26,4 @@ check_command "GCC" "gcc"
 check_command "G++" "g++"
 check_command "Rust" "rustc"
 check_command "Cargo" "cargo"
+check_command "Go" "go" "version"
