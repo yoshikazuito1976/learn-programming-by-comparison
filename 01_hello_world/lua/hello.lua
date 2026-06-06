704c61f4a1e884617c56a41cabdaf5e5a8852698
Yoshikazu ITO <aisyu1976@gmail.com>
2026-06-06 09:30:17 +0900

    Add Lua hello example and environment check

---
+++ 01_hello_world/lua/hello.lua
@@ -0,0 +1,11 @@
+print("Hello, Lua!")
+
+local name = "Yoshikazu"
+print("Hello, " .. name .. "!")
+
+local language = "Lua"
+print("This language is " .. language .. ".")
+
+local age = 49
+
+print(string.format("Hello, %s! You are %d years old.", name, age))
