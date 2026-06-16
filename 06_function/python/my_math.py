9b43e9e2b4f9dbe0889a746689a97b05240b3405
Yoshikazu ITO <aisyu1976@gmail.com>
2026-06-17 07:01:10 +0900

    Add Python function examples

---
+++ 06_function/python/my_math.py
@@ -0,0 +1,6 @@
+# my_math.py
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
---
+++ 06_function/python/use_my_math.py
@@ -0,0 +1,16 @@
+# use_my_math.py
+import my_math #同じディレクトリ内にあるmy_math.pyをインポートする
+
+print(my_math.add(1, 2))
+print(my_math.sub(5, 3))
+
+'''
+This is a simple example of using a custom math module.
+import は「ファイル名を書けば何でも読める」わけではありません。
+Python は、決められた場所からモジュールを探します。
+
+pip で入れた外部モジュールは、Python が探せる場所に入るので import しやすいです。
+しかし、自分で作った .py ファイルは、置き場所によって import できたりできなかったりします。
+
+'''
+
